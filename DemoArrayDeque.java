78ef7df052a28bc911268120de59c9fa5264098c
Anusha Balasani <balasanianusha24@gmail.com>
2024-06-30 15:27:10 +0530

    Add files via upload

--- DemoArrayDeque.java
+++ DemoArrayDeque.java
@@ -1,31 +1,30 @@
-package apj;
+package com.collections;
+
 import java.util.ArrayDeque;
 
 public class DemoArrayDeque  {
-
- public static void main(String[] args) {
-	 String que=new String("Names");
-	 System.out.println(que);
-	 
-    ArrayDeque queue = new ArrayDeque();
-   //add values to queue
-    queue.add("Sandya");
-    queue.add("Aruna");
-    queue.add("Jalaja");
-    queue.add("Moksha");
-	//print the values using poll and peak methods	 
-    System.out.println(queue);
-	System.out.println(queue.poll());
-    System.out.println(queue.peek());
-    String que1=new String("Collection of Names");
-    queue.addFirst("Vasanti");
-    queue.add("Sakhi");
-    System.out.println(que1.compareTo(que));
-   
-    queue.add("Kavya");
-    System.out.println(queue);
-  }
+    public static void main(String[] args) {
+		ArrayDeque<String> queue = new ArrayDeque<String>();
+	    //add values to queue
+		 queue.add("Sandya");
+		 queue.add("Aruna");
+		 queue.add("Jalaja");
+		 queue.add("Moksha");
+		 //print the values using poll and peak methods	 
+		 System.out.println(queue);
+	     System.out.println(queue.poll());
+		 System.out.println(queue.peek());
+		 System.out.println(queue);
+		 System.out.println(queue.size());
+		 System.out.println(queue.remove("Aruna"));
+		 System.out.println(queue.isEmpty());
+		 System.out.println(queue.pop());
+		
+		 System.out.println(queue);
+    }
 
 }
+
+	
 
 
