4b1cc465c38df22c5e665647ef70f63e039311cc
Anusha Balasani <balasanianusha24@gmail.com>
2024-06-30 14:16:31 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,23 @@
+package apj;
+//programee using operaters
+public class Operator {
+
+	    public static void main(String[] args) {
+	    int a=10;
+	    int b=20;
+
+	
+	
+	    //print  the  values
+		System.out.println("a>b=" +(a>b));
+		System.out.println("a<b=" +(a<b));
+	    System.out.println("a>=b=" +(a>=b));
+		System.out.println("a<=b=" +(a<=b));
+		System.out.println("a!=b=" +(a!=b));
+
+		System.out.println("a+b=" +(a+b));
+		System.out.println("a*b=" +(a*b));
+		System.out.println("a%b=" +(a%b));
+	    System.out.println("a/b=" +(a/b));
+}
+}
